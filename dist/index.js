182a784fdfc904d0009d7771b1c9f486471ae6df
Eddwin Paz <pazeddwin@gmail.com>
2020-12-05 16:38:37 -0300

    added port

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 const app = require("./app");
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     /* eslint-disable no-console */
     console.log(`Listening: http://localhost:${port}`);
